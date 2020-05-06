7f6dc17492ffcda98b0cf08ceac2a0061367767f
ge tang <ge.tang@icloud.com>
2020-05-06 21:23:17 +0200

    Add the pre_post measurements

---
+++ pre_post/BMI.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 30 17:10:26 2020
+
+@author: Ethan
+
+Markers:
+    bmi_start
+
+    prepare
+    nothing, imagine, or move
+    relax
+
+    bmi_end
+"""
+
+
+def bmi_main(canvas):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import time, reiz, liesl
+    from reiz.visual import Mural, Background
+
+    def countdown(canvas, sek):
+        for i in range(0, sek):
+            cue = reiz.Cue(canvas, visualstim=[bg, Mural(text=str(sek - i), color=(0.18, 0.18, 0.18))])
+            cue.show(duration=1)
+
+    def part2(cuetype, image_lib):
+        if "Nothing" in cuetype:
+            DispImage = image_lib.Nothing
+        elif "Imagine" in cuetype:
+            DispImage = image_lib.Imagine
+        elif "Open" in cuetype:
+            DispImage = image_lib.Open
+        elif "Close" in cuetype:
+            DispImage = image_lib.Close
+        return DispImage
+
+    bg          = Background(color='gray')
+    states      = ("Nothing", "Imagine", "Open", "Close")
+    image_lib   = reiz.visual.read_folder(r'C:\projects\pranayama')
+    nBlocks     = 3
+    tiles       = np.tile(states, (4))
+    block_tiles = np.tile(states, (nBlocks, 4))
+    for i in range(nBlocks):
+        block_tiles[i, :] = np.random.permutation(tiles)
+
+    canvas.start_run = False
+    start_protocol = reiz.Cue(
+        canvas, visualstim=[bg, Mural(text='Press F5 to start BMI', color=(0.18, 0.18, 0.18))])
+    while not canvas.start_run:
+        start_protocol.show(duration=0.1)
+    countdown(canvas, 3)
+
+
+    reiz.Cue(canvas,visualstim=[bg, reiz.visual.Mural("BMI Task:", position=[0, 0.5], fontsize=1.5, color=(0.18, 0.18, 0.18)),
+        reiz.visual.Mural("Bitte folgen Sie den Anweisungen", position=[0, -0.25], fontsize=1, color=(0.18, 0.18, 0.18))]).show(duration=5)
+
+    reiz.Cue(canvas,visualstim=[bg, reiz.visual.Mural("Bilder werden angezeigt.", position=[0, 0.4], fontsize=1, color=(0.18, 0.18, 0.18)),
+                                reiz.visual.Mural("Bitte 3 Sekunden lang durchführen", position=[0, -0.4], fontsize=1, color=(0.18, 0.18, 0.18))]).show(5)
+
+    reiz.Cue(canvas,visualstim=[bg, image_lib.Open, reiz.visual.Mural("Öffnen Ihre rechte Hand", position=[0, 0.7], fontsize=1, color=(0.18, 0.18, 0.18))]).show(5)
+    reiz.Cue(canvas,visualstim=[bg, image_lib.Close, reiz.visual.Mural("Schließe Ihre rechte Hand", position=[0, 0.7], fontsize=1, color=(0.18, 0.18, 0.18))]).show(5)
+    reiz.Cue(canvas,visualstim=[bg, image_lib.Imagine,
+                                reiz.visual.Mural("Stellen sich vor Ihre rechte Hand zu öffnen", position=[0, 0.7], fontsize=0.7, color=(0.18, 0.18, 0.18))]).show(5)
+    reiz.Cue(canvas,visualstim=[bg, image_lib.Nothing,
+                                reiz.visual.Mural("Mach nichts", position=[0, 0.7], fontsize=1, color=(0.18, 0.18, 0.18))]).show(10)
+
+    reiz.marker.push('bmi_start')
+    for k in range(nBlocks):
+
+        canvas.start_run = False
+        start_protocol = reiz.Cue(
+            canvas, visualstim=[bg, Mural(text="Press F5 to start block " + str(k + 1), color=(0.18, 0.18, 0.18))])
+
+        while not canvas.start_run:
+            start_protocol.show(duration=0.1)
+        countdown(canvas, 3)
+
+        for cue in range(np.size(block_tiles, 1)):
+            reiz.marker.push("prepare_" + str(k) + '_' + str(cue))
+            reiz.Cue(canvas, visualstim=[bg, reiz.visual.Mural("Bereitmachen", position=[0, 0.4], fontsize=1, color=(0.18, 0.18, 0.18))]).show(3)
+
+            reiz.marker.push(str(block_tiles[k,cue]) + '_' + str(k) + '_' + str(cue))
+            reiz.Cue(canvas, visualstim=[bg, part2(block_tiles[k,cue], image_lib)]).show(3)
+
+            reiz.marker.push("relax_" + str(k) + '_' + str(cue))
+            reiz.Cue(canvas, visualstim=[bg, reiz.visual.Mural("Entspannen", position=[0, -0.4], fontsize=1, color=(0.18, 0.18, 0.18))]).show(5)
+
+    reiz.marker.push('bmi_end')
---
+++ pre_post/hwave/HR_GUI.py
@@ -0,0 +1,182 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 09:28:37 2020
+
+@author: Ethan
+"""
+from liesl.files.session import Session
+from stg.api import STG4000
+from PyQt5 import QtWidgets
+from prana.pre_post.hwave import test_stimulation
+import sys
+from functools import partial
+from reiz.marker import push
+import time
+from scipy import stats
+import numpy as np
+import liesl
+import configparser
+import matplotlib
+from matplotlib.backends.qt_compat import is_pyqt5
+
+if is_pyqt5():
+    from matplotlib.backends.backend_qt5agg import (
+        FigureCanvas,
+        NavigationToolbar2QT as NavigationToolbar,
+    )
+else:
+    from matplotlib.backends.backend_qt4agg import (
+        FigureCanvas,
+        NavigationToolbar2QT as NavigationToolbar,
+    )
+
+# Ensure using PyQt5 backend
+matplotlib.use("QT5Agg")
+
+def on_close():
+   print("Shutting down")
+
+def create_stimulation_signal(intensity):
+    amplitudes_in_mA = [intensity / 1000]
+    durations_in_ms = [float("1")]
+    return amplitudes_in_mA, durations_in_ms
+
+class Ui(QtWidgets.QMainWindow, test_stimulation.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(Ui, self).__init__(parent)
+        self.setupUi(self)
+        self.show()
+
+        self.FDS_R = int(cfg["H_reflex"]["fds_r"])
+        sinfo = liesl.get_streaminfos_matching(type="EEG")[0]
+        self.buffer = liesl.RingBuffer(sinfo, duration_in_ms=500)
+        self.buffer.start()
+        self.buffer.await_running()
+        time.sleep(1)
+
+        self.addToolBar(NavigationToolbar(self.MplWidget.canvas, self))
+
+        self.download()
+        self.intensity_minus_1000.clicked.connect(
+            partial(self.decrease_intensity, 1000))
+        self.intensity_plus_1000.clicked.connect(partial(self.increase_intensity, 1000))
+        self.intensity_minus_100.clicked.connect(partial(self.decrease_intensity, 100))
+        self.intensity_plus_100.clicked.connect(partial(self.increase_intensity, 100))
+        self.trigger.clicked.connect(self.start_stimulation)
+
+        push("hreflex_start")
+
+    def plot_hreflex(self):
+        data    = self.buffer.get_data()[:, self.FDS_R]
+        peakpos = [data.argmin(), data.argmax()]
+        val     = [data.min(), data.max()]
+        vpp     = val[1] - val[0]
+        wndw    = [np.min(peakpos) - 100, np.max(peakpos) + 100]
+        timey   = np.arange(0, len(data), 1)
+
+        # discards the old graph
+        self.MplWidget.canvas.axes.clear()
+
+        # plot data
+        self.MplWidget.canvas.axes.plot(
+            timey[wndw[0] : wndw[1]], data[wndw[0] : wndw[1]])
+
+        self.MplWidget.canvas.axes.vlines(
+            [timey[peakpos[0]]], val[0], np.mean(data), color="red", linestyle="dashed")
+        self.MplWidget.canvas.axes.vlines(
+            [timey[peakpos[1]]], val[1], np.mean(data), color="red", linestyle="dashed")
+
+        textstr = "Vpp = {0:3.2f}".format(vpp)
+        props   = dict(boxstyle="round", facecolor="wheat", alpha=0.5)
+
+        self.MplWidget.canvas.axes.text(
+            0.05,
+            0.95,
+            textstr,
+            transform=self.MplWidget.canvas.axes.transAxes,
+            fontsize=14,
+            verticalalignment="top",
+            bbox=props)
+        time.sleep(0.01)
+
+        # refresh canvas
+        self.MplWidget.canvas.draw()
+
+    def download(self):
+        self.cur = int(self.intensity_label.text())
+        amplitudes_in_mA, durations_in_ms = create_stimulation_signal(self.cur)
+        self.mcs.download(0, amplitudes_in_mA, durations_in_ms)
+
+    def increase_intensity(self, factor=20):
+        cur = int(self.intensity_label.text())
+        cur += factor
+        if cur > 16000:
+            cur = 0
+        self.intensity_label.setText(str(cur))
+        self.download()
+
+    def decrease_intensity(self, factor=20):
+        cur = int(self.intensity_label.text())
+        cur -= factor
+        if cur < 0:
+            cur = 16000
+        self.intensity_label.setText(str(cur))
+        self.download()
+
+    def start_stimulation(self):
+        push("h_reflex_stim_" + str(self.cur / 1000))
+        self.mcs.start_stimulation()
+        time.sleep(0.25)
+        self.plot_hreflex()
+
+    @property
+    def mcs(self):
+        if not hasattr(self, "_mcs"):
+            mcs = None
+            while mcs is None:
+                try:
+                    mcs = STG4000()
+                except IndexError:  # no STG connected
+                    self.error_missing_stg()
+            self._mcs = mcs
+            self.setWindowTitle("Selected " + str(self.mcs))
+        return self._mcs
+
+    def execute_right_click(self):
+        try:
+            amount = int(self.rc_amount.text())
+        except ValueError:
+            amount = 1000
+        if self.rc_dec.isChecked():
+            self.decrease_intensity(amount)
+        elif self.rc_inc.isChecked():
+            self.increase_intensity(amount)
+
+    def mousePressEvent(self, QMouseEvent):
+        button = ["left", "right"][QMouseEvent.button() - 1]
+        if button == "right":
+            self.execute_right_click()
+
+    def closeEvent(self, event):
+        push("hreflex_end")
+        session.stop_recording()
+        app.aboutToQuit.connect(on_close)
+
+# %%
+import reiz
+reiz.marker.start()
+        
+if __name__ == "__main__":
+    cfg = configparser.ConfigParser()
+    cfg.read(r"C:\tools\study-breathing_intervention\prana\cfg.ini")
+    with open(r"C:\tools\study-breathing_intervention\prana\cfg.ini", "w",) as configfile:
+        cfg.write(configfile)
+    streamargs = [{'name':"eego"}, {'name':'reiz-marker'}, {'name':'GDX-RB_0K2002A1'}, {'name':'localite_marker'}, {'name':'pupil_capture'}]
+    session    = Session(prefix=cfg['study_info']['subject_token'], streamargs=streamargs)
+    time.sleep(0.01)
+
+    with session('h_reflex'):
+        app = QtWidgets.QApplication(sys.argv)
+        window = Ui()
+        window.show()
+        app.exec_()
